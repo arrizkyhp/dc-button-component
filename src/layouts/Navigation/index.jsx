677dc9a2e1bcbc39365e892c12a1e7aec2548f33
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-10-26 10:18:24 +0700

    Update navigation logo link

--- src/layouts/Navigation/index.jsx
+++ src/layouts/Navigation/index.jsx
@@ -2,42 +2,44 @@ import React from 'react'
 
 export default function Navigation() {
     return (
-        <nav className="navigation">
-            <h1 className="navigation__logo">
-                <a href="/">Dev<span>challenges.io</span></a>
-            </h1>
-            <ul className="navigation__menu">
-                <li className="navigation__menu__list">
-                    <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
-                        Colors
-                    </a>
-                </li>
-                <li className="navigation__menu__list">
-                    <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
-                        Typography
-                    </a>
-                </li>
-                <li className="navigation__menu__list">
-                    <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
-                        Spaces
-                    </a>
-                </li>
-                <li className="navigation__menu__list">
-                    <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
-                        Buttons
-                    </a>
-                </li>
-                <li className="navigation__menu__list">
-                    <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
-                        Inputs
-                    </a>
-                </li>
-                <li className="navigation__menu__list">
-                    <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
-                        Grid
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    )
+      <nav className="navigation">
+        <h1 className="navigation__logo">
+          <a href="https://devchallenges.io/">
+            Dev<span>challenges.io</span>
+          </a>
+        </h1>
+        <ul className="navigation__menu">
+          <li className="navigation__menu__list">
+            <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
+              Colors
+            </a>
+          </li>
+          <li className="navigation__menu__list">
+            <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
+              Typography
+            </a>
+          </li>
+          <li className="navigation__menu__list">
+            <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
+              Spaces
+            </a>
+          </li>
+          <li className="navigation__menu__list">
+            <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
+              Buttons
+            </a>
+          </li>
+          <li className="navigation__menu__list">
+            <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
+              Inputs
+            </a>
+          </li>
+          <li className="navigation__menu__list">
+            <a href="/" target="_blank" rel="noopener noreferrer" data-text="title">
+              Grid
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
 }
