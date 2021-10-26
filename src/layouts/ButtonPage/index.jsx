78b2b7691b69b84e7101c62684505b8f1ba61da4
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-10-26 09:41:48 +0700

    done slicing

--- src/layouts/ButtonPage/index.jsx
+++ src/layouts/ButtonPage/index.jsx
@@ -67,13 +67,77 @@ export default function ButtonPage() {
         <div className="button-page__group btn-icon-area">
           <div className="button-page__group__content">
             <p className="button-text">&lt;Button startIcon=”add_shopping_cart” /&gt;</p>
-            <Button startIcon="add_shopping_cart" color="primary">Default</Button>
+            <Button startIcon="add_shopping_cart" color="primary">
+              Default
+            </Button>
           </div>
           <div className="button-page__group__content">
             <p className="button-text">&lt;Button endIcon=”add_shopping_cart” /&gt;</p>
-            <Button endIcon="add_shopping_cart" color="primary">Default</Button>
+            <Button endIcon="add_shopping_cart" color="primary">
+              Default
+            </Button>
           </div>
         </div>
+
+        <div className="button-page__group btn-size-area">
+          <div className="button-page__group__content">
+            <p className="button-text">&lt;Button size=”sm” /&gt;</p>
+            <Button color="primary" size="sm">
+              Default
+            </Button>
+          </div>
+          <div className="button-page__group__content">
+            <p className="button-text">&lt;Button size=”md” /&gt;</p>
+            <Button color="primary" size="md">
+              Default
+            </Button>
+          </div>
+          <div className="button-page__group__content">
+            <p className="button-text">&lt;Button size=”lg” /&gt;</p>
+            <Button color="primary" size="lg">
+              Default
+            </Button>
+          </div>
+        </div>
+
+        <div className="button-page__group btn-color-area">
+          <div className="button-page__group__content">
+            <p className="button-text">&lt;Button color="default" /&gt;</p>
+            <Button color="default">Default</Button>
+          </div>
+          <div className="button-page__group__content">
+            <p className="button-text">&lt;Button color="primary" /&gt;</p>
+            <Button color="primary">Default</Button>
+          </div>
+          <div className="button-page__group__content">
+            <p className="button-text">&lt;Button color="secondary" /&gt;</p>
+            <Button color="secondary">Default</Button>
+          </div>
+          <div className="button-page__group__content">
+            <p className="button-text">&lt;Button color="danger"/&gt;</p>
+            <Button color="danger">Default</Button>
+          </div>
+        </div>
+
+        <div className="button-page__group btn-color-area">
+          <div className="button-page__group__content">
+            <p className="button-text button-text__hover">&:hover, &:focus</p>
+            <Button color="default" className="btn-hover">Default</Button>
+          </div>
+          <div className="button-page__group__content btn-color-hover">
+            <Button color="primary" className="btn-hover">Default</Button>
+          </div>
+          <div className="button-page__group__content btn-color-hover">
+            <Button color="secondary" className="btn-hover">Default</Button>
+          </div>
+          <div className="button-page__group__content btn-color-hover">
+            <Button color="danger" className="btn-hover">Default</Button>
+          </div>
+        </div>
+
+        <p className="attribution">
+          created by <a href="https://github.com/arrizkyhp/" target="_blank" rel="noreferrer"><b>arrizkyhp</b></a> - devChallenges.io
+        </p>
       </section>
     );
 }
